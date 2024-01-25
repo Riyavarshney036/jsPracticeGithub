d6d67b66afae3cdb14f1b32cff4cde23bd801d3f
Riyavarshney036 <riyavarshney036@gmail.com>
2024-01-25 14:17:29 +0530

    first commit

---
+++ if-else/comparisonBetweenTwoNumber.js
@@ -0,0 +1,28 @@
+let a = 2
+let b = 65
+let c = 23
+let d = 28
+
+if(a>b){
+    
+    if(a>c){
+        if(a>d){
+            console.log("a is greater")
+        }else{
+            console.log("d is greater")
+        }
+    }else{
+        console.log("c is greater")
+    }
+}else{
+    
+    if(b>c){
+        if(b>d){
+            console.log("b is greater")
+        }else{
+            console.log("d is greater")
+        }
+    }else{
+        console.log("c is greater")
+    }
+}
